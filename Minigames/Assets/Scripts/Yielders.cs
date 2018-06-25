bb218a14f8e9df409dc21b563c4f754c8f08fcfb
Alexander Xu <ajm.xu@outlook.com>
2018-06-25 16:19:02 +0100

    Added ThrowIn Minigame
    - Added the throw in minigame with fractions, decimals and percentages
    - Animations still need to be added

---
+++ Minigames/Assets/Scripts/Yielders.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+ 
+public static class Yielders {
+
+	private static Dictionary<float, WaitForSeconds> timeInterval = new Dictionary<float, WaitForSeconds>();
+	private static Dictionary<float, WaitForSecondsRealtime> timeIntervalRealTime = new Dictionary<float, WaitForSecondsRealtime>();
+
+	public static WaitForSeconds Get(float seconds) {
+		if (!timeInterval.ContainsKey(seconds)) {
+			timeInterval.Add(seconds, new WaitForSeconds(seconds));
+		}
+		
+		return timeInterval[seconds];
+	}
+
+	public static WaitForSecondsRealtime GetRealTime(float seconds) {
+		if (!timeIntervalRealTime.ContainsKey(seconds)) {
+			timeIntervalRealTime.Add(seconds, new WaitForSecondsRealtime(seconds));
+		}
+		
+		return timeIntervalRealTime[seconds];
+	}
+}
